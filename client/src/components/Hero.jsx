e8c3c586096b24fed6cad8c749a9f2343e3563de
RY18coder <148080068+RY18coder@users.noreply.github.com>
2026-06-16 12:36:49 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
         <div className='text-center mb-6'>
             <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br/> with <span className='text-primary'>AI tools</span></h1>
-            <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'> Transform your workflow with premium AI. Write articles, design images, and accelerate your content production effortlessly.</p>
+            <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'> Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.</p>
         </div>
 
         <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
@@ -19,7 +19,7 @@ const Hero = () => {
             <button className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
         </div>
         <div className='flex items-center gap-4 mt-8 mx-auto text-gray-600'>
-            <img src={assets.user_group} alt="" className='h-8'/> Trusted by 5k+ people
+            <img src={assets.user_group} alt="" className='h-8'/> Trusted by 10k+ people
         </div>
       
     </div>
